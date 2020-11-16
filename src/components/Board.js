1c1050fa30dfe7980b7114d5ca72f431afd2e969
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2020-11-16 21:47:36 +0100

    Add: add more functions and states to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,37 @@
 import react from "react";
 import Tile from ".Tile";
 import { BOARD_SIZE, GRID_SIZE, TILE_COUNT } from "../constants";
+import { canSwap, shuffle, swap } from "../helpers";
 const Board = () => {
-    // Keep track of the tiles in states
+    // Keep track of the tiles in states, gives an array of correct size
     const[tiles, setTiles] = useState([...Array(TILE_COUNT).keys]);
+    const [isSolved, setIsSolved] = useState(false);
+    const [isStarted, setIsStarted] = useState(false);
+
+    // sets the state to the shuffled tiles from the helper functions
+    const shuffleTiles = () => {
+        const shuffledTiles = shuffle(tiles)
+        setTiles(shuffle)
+    }
+
+    // argument is the tile that has been clicked
+    const swapTiles = (tileIndex) => {
+        if(canSwap(tileIndex, tile.length -1 )) {
+            const swappedTiles = swap(tiles, tileIndex, tiles.length -1)
+            setTiles(swappedTiles)
+        }
+    }
+     
+    //Handle clicks
+    const handleTileClick = (index) => {
+        swapTiles(index)
+    }
+
+    const handleShuffleClick = () => {
+        shuffleTiles()
+    }
+
+
     // Based on board size
     const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
     const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
