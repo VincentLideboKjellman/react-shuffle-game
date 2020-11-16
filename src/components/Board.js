ce97ba4001d61b4899a5a259651143942f36512b
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2020-11-16 18:48:29 +0100

    Add: Create Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,33 @@
-import { isExpressionWrapper } from '@babel/types';
-import react from 'react';
-
+import react from "react";
+import Tile from ".Tile";
+import { BOARD_SIZE, GRID_SIZE, TILE_COUNT } from "../constants";
 const Board = () => {
+    // Keep track of the tiles in states
+    const[tiles, setTiles] = useState([...Array(TILE_COUNT).keys]);
+    // Based on board size
+    const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
+    const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
+    const style = {
+        width: BOARD_SIZE,
+        height: BOARD_SIZE,
+    };
+
     return (
-      <div >
-          TEST
-      </div>
+    <>
+        <ul style={style} className="board">
+            {tiles.map((tile, index) => (
+            <Tile
+                key={tile}
+                index={index}
+                tile={tile}
+                width={pieceWidth}
+                height={pieceHeight}
+                />
+            ))}
+        </ul>
+        <button>slumpa</button>
+    </>
     );
-  }
+}
 
 export default Board;
