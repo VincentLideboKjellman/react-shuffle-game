e0dc27f3600481a50d128faa11eeb39cc8506a1a
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2020-11-16 16:20:42 +0100

    Chore: remove trash and add more files

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import { isExpressionWrapper } from '@babel/types';
+import react from 'react';
+
+const Board = () => {
+    return (
+      <div >
+          TEST
+      </div>
+    );
+  }
+
+export default Board;
